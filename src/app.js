630efd428f1adc30c663f86181794b12cb20a4f0
Kien Dinh Huu <80394199+dustindinh-dev@users.noreply.github.com>
2023-12-12 23:56:00 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -85,7 +85,7 @@ async function run() {
 
   // Run the server!
   try {
-    await fastify.listen({ port: process.env.PORT || 3000 })
+    await fastify.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
